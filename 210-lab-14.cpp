5ffe62febe29d46f638ae57618da63e912614517
Hussain <Aahil.Hussain@ejgallo.com>
2024-09-29 15:41:08 -0700

    Second commit

---
+++ 210-lab-14.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Color {
+    private:
+        int red;
+        int green;
+        int blue;
+
+public:
+    Color(int r, int g, int b) : red(r), green(g), blue(b) {}
+    void print() {
+        std::cout << "Color(" << red << ", " << green << ", " << blue << ")\n";
+    }
+};
+
+int main(){
+    return 0;
+}
+
+setred(){
+    red = 0;
+}
+
+print(){
+    
+}
