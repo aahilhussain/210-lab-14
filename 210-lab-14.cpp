d2dc67abe655826dc0d6d4450fe586d0946e739d
Hussain <Aahil.Hussain@ejgallo.com>
2024-09-29 15:54:47 -0700

    3rd Committ

--- 210-lab-14.cpp
+++ 210-lab-14.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 
+//color object
 class Color {
     private:
         int red;
         int green;
         int blue;
 
+//creating print function
 public:
     Color(int r, int g, int b) : red(r), green(g), blue(b) {}
+    void setRed(int r)              {red = r; }
+    void setGreen (int g)           {green = g;}
+    void setBlue (int b)            {blue = b;}
+    int getRed()                    {return red;}
+    int getGreen()                  {return green;}
+    int getBlue()                   {return blue;}
     void print() {
         std::cout << "Color(" << red << ", " << green << ", " << blue << ")\n";
     }
 };
 
+//displaying print function
 int main(){
+    Color color1(255, 0, 0);
+    color1.print();
     return 0;
-}
-
-setred(){
-    red = 0;
-}
-
-print(){
-    
 }
